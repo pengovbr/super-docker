dd3fb588b67c8337132ab5f8a2bfc61ccbff36fd
Marcelo Linhares Castro <marlinhares@gmail.com>
2022-05-16 10:33:01 -0300

    Mais uma tonelada de adicoes para ser apreciada/criticada para a versao inicial

---
+++ containers/app/app-ci/assets/scripts-e-automatizadores/clone-modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p /sei-modulos
+
+cd /sei-modulos
+
+git clone https://github.com/spbgovbr/mod-sei-estatisticas.git
+git clone https://github.com/spbgovbr/mod-sei-pen.git
+git clone https://github.com/spbgovbr/mod-wssei.git
+git clone https://github.com/spbgovbr/mod-sei-resposta.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/spbgovbr/mod-gestao-documental.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/spbgovbr/mod-sei-loginunico.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/spbgovbr/mod-sei-assinatura-avancada.git
--- containers/databases/sqlserver/assets/install.sh
+++ containers/databases/sqlserver/assets/install.sh
@@ -3,14 +3,17 @@ set -e
 
 # Variáveis de ambiente
 export ACCEPT_EULA=Y
-export SA_PASSWORD='P@ssword'
+export SA_PASSWORD='yourStrong(!)Password'
 
 # Instalação do FreeTDS para acesso ao SQL Server
-apt-get -y --allow-unauthenticated update
-apt-get -y --allow-unauthenticated install libodbc1 freetds-dev freetds-bin
+#apt-get -y --allow-unauthenticated update
+#apt-get -y --allow-unauthenticated install libodbc1 freetds-dev freetds-bin
 
 /opt/mssql/bin/sqlservr &
 sleep 30
+
+mv /tmp/sip_super_4.0.x.1_BD_Ref_Exec.bak /tmp/sip_sqlserver.bak
+mv /tmp/sei_super_4.0.x.1_BD_Ref_Exec.bak /tmp/sei_sqlserver.bak
 
 tsql -S localhost -U sa -P $SA_PASSWORD < /tmp/restore.sql
 
